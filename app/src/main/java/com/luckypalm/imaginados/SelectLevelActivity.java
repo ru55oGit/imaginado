e92163b7c4bcae2b7478886a8da997e8bc99a1ee
Patricio Toledo <Russo@iMac.local>
2016-11-03 21:09:08 -0300

    select level

--- app/src/main/java/com/luckypalm/imaginados/SelectLevelActivity.java
+++ app/src/main/java/com/luckypalm/imaginados/SelectLevelActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SelectLevelActivity extends AppCompatActivity {
-    private ImageView play;
+    private TextView play;
+    private LinearLayout contenedorNiveles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +25,35 @@ public class SelectLevelActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_select_level);
 
-        play = (ImageView) findViewById(com.luckypalm.imaginados.R.id.play);
+        /*play = (TextView) findViewById(com.luckypalm.imaginados.R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SelectLevelActivity.this, SelectImagesActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
+
+        contenedorNiveles = (LinearLayout) findViewById(R.id.innerLay);
+
+        for (int i = 1;i<=20;i++) {
+            TextView letter = new TextView(this);
+            letter.setGravity(Gravity.CENTER_HORIZONTAL);
+            int dim = 210;
+            letter.setTextSize((int)getResources().getDimension(com.luckypalm.imaginados.R.dimen.margin_clock));
+            letter.setBackground(getResources().getDrawable(R.drawable.circle));
+            letter.setText(i+"");
+            LinearLayout.LayoutParams marginLetters = new LinearLayout.LayoutParams(dim, dim);
+            if (i%2 == 0) {
+                marginLetters.setMargins(0, 25, 30, 0);
+            } else {
+                marginLetters.setMargins(0, 0, 30, 25);
+            }
+
+            letter.setLayoutParams(marginLetters);
+
+            contenedorNiveles.addView(letter);
+        }
     }
 
     @Override
