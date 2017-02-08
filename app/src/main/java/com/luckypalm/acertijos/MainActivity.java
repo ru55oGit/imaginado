ca9bbd7d8744c7b9b1380c9e70789aaf54012dd1
Patricio Toledo <patricio.toledo@wunderman.com>
2017-02-08 16:03:09 -0300

    anulando el timer en el click de la home

--- app/src/main/java/com/luckypalm/acertijos/MainActivity.java
+++ app/src/main/java/com/luckypalm/acertijos/MainActivity.java
@@ -2,15 +2,13 @@ package com.luckypalm.acertijos;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-
-import com.luckypalm.acertijos.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                timer.cancel();
                 Intent intent = new Intent(MainActivity.this, SelectLevelActivity.class);
                 startActivity(intent);
             }
