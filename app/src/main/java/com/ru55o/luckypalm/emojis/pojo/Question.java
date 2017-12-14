2751bf538058f70d7271652aac1ea604cdb0ffed
Toledo <patricio.toledo@yrbrands.com>
2017-12-14 09:02:56 -0300

    cambios de estilos, imagenes emojis

---
+++ app/src/main/java/com/ru55o/luckypalm/emojis/pojo/Question.java
@@ -0,0 +1,34 @@
+package com.ru55o.luckypalm.emojis.pojo;
+
+/**
+ * Created by toledop on 13/07/2016.
+ */
+public class Question {
+    private String pregunta;
+    private String respuesta;
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+}
