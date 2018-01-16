2261aa2ea40711ba599edd198895b2794efc2556
Toledo <patricio.toledo@yrbrands.com>
2018-01-16 15:06:21 -0300

    cambio imagenes marvel y simpsons

--- app/src/main/java/com/ru55o/luckypalm/preguntas/PlayActivity.java
+++ app/src/main/java/com/ru55o/luckypalm/preguntas/PlayActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
@@ -26,6 +28,8 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_play);
 
         settings = getSharedPreferences("Status", 0);
