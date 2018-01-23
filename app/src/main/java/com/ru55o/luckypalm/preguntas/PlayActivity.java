61900373c16314a370b817a9e739082d2fbd606c
Toledo <patricio.toledo@yrbrands.com>
2018-01-23 14:48:07 -0300

    imagen segun nivel jugado en playactivity

--- app/src/main/java/com/ru55o/luckypalm/preguntas/PlayActivity.java
+++ app/src/main/java/com/ru55o/luckypalm/preguntas/PlayActivity.java
@@ -3,6 +3,8 @@ package com.ru55o.luckypalm.preguntas;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -110,6 +112,57 @@ public class PlayActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT > 16) {
+            if (!languageSwitch.isChecked()) {
+                emojis.setBackground(getImage("emojis", Integer.parseInt(getLevelByCategory("emojis"))));
+            } else {
+                emojis.setBackground(getImage("enojis", Integer.parseInt(getLevelByCategory("enojis"))));
+            }
+
+            if (!languageSwitch.isChecked()) {
+                acertijos.setBackground(getImage("adivinanzas", Integer.parseInt(getLevelByCategory("adivinanzas"))));
+            } else {
+                acertijos.setBackground(getImage("wuzzles", Integer.parseInt(getLevelByCategory("wuzzles"))));
+            }
+
+            if (!languageSwitch.isChecked()) {
+                peliculas.setBackground(getImage("peliculas", Integer.parseInt(getLevelByCategory("peliculas"))));
+            } else {
+                peliculas.setBackground(getImage("movies", Integer.parseInt(getLevelByCategory("movies"))));
+            }
+        }
+    }
+
+    public String getLevelByCategory(String cat){
+        String result = "";
+        if ("adivinanzas".equals(cat)) {
+            result = settings.getString("levelAdivinanzas","1");
+        } else if ("wuzzles".equals(cat)) {
+            result = settings.getString("levelWuzzles","1");
+        } else if ("emojis".equals(cat)) {
+            result = settings.getString("levelEmojis","1");
+        } else if ("enojis".equals(cat)) {
+            result = settings.getString("levelEnojis","1");
+        } else if ("peliculas".equals(cat)) {
+            result = settings.getString("levelPeliculas","1");
+        } else if ("movies".equals(cat)) {
+            result = settings.getString("levelMovies","1");
+        }
+
+        return result;
+    }
+    public Drawable getImage(String categorySelected, int i) {
+        Drawable backgroundLevel;
+
+        int res = getResources().getIdentifier(categorySelected + i, "drawable", getPackageName());
+        backgroundLevel = getResources().getDrawable(res);
+
+        return backgroundLevel;
     }
 
 }
